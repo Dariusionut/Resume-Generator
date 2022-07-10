ddb9829b03f9a800514a5be734a58ace5a83e67c
Darius96 <dariustinculescu@gmail.com>
2022-07-11 02:52:13 +0300

    Changed classes name

--- src/main/java/com/spring/cvgenerator/CvGeneratorApplication.java
+++ src/main/java/com/spring/cvgenerator/CvGeneratorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CvgeneratorApplication {
+public class CvGeneratorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CvgeneratorApplication.class, args);
+        SpringApplication.run(CvGeneratorApplication.class, args);
     }
 
 }
