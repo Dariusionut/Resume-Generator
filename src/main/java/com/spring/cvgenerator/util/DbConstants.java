3b381988c21aa1b28f25941d7bb46fdd5e3cdd5b
Darius96 <dariustinculescu@gmail.com>
2022-07-11 03:03:29 +0300

    Added DbConstants and AppConstants

---
+++ src/main/java/com/spring/cvgenerator/util/DbConstants.java
@@ -0,0 +1,56 @@
+package com.spring.cvgenerator.util;
+
+public class DbConstants {
+
+    public static final String SCHEMA_PUBLIC = "public";
+    /**
+     * Tables
+     */
+    public static final String TABLE_APP_USER = "app_user";
+    public static final String TABLE_GENDER = "gender";
+    public static final String TABLE_ROLE = "role";
+    public static final String TABLE_USER_ROLE = "user_role";
+
+    /**
+     * Entities
+     */
+
+    public static final String ENTITY_APP_USER = "AppUser";
+    public static final String ENTITY_GENDER = "Gender";
+    public static final String ENTITY_ROLE = "Role";
+
+    /**
+     * Sequences
+     */
+
+    public static final String SEQUENCE_APP_USER = "sequence_app_user";
+    public static final String SEQUENCE_GENDER = "sequence_gender";
+    public static final String SEQUENCE_ROLE = "sequence_role";
+
+    /**
+     * Columns
+     */
+
+    public static final String ID = "id";
+    public static final String USER_ID = "user_id";
+    public static final String GENDER_ID = "gender_id";
+    public static final String ROLE_ID = "role_id";
+    public static final String DATE_ADDED = "date_added";
+    public static final String VERSION = "version";
+    public static final String IS_ENABLED = "is_enabled";
+
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String DOB = "dob";
+    public static final String USERNAME = "username";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String GENDER_NAME = "gender_name";
+    public static final String ROLE_NAME = "role_name";
+
+    /**
+     * Other
+     */
+    public static final boolean TRUE = true;
+    public static final boolean FALSE = false;
+}
